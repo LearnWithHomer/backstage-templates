fc8257bb7e397fd9ad2c183f58c9aeda9500ab3e
aymaks <ayoola.makinde@homerlearning.com>
2024-06-20 14:22:54 -0400

    simplifying

--- simple-ec2-template/infrastructure/main.tf
+++ simple-ec2-template/infrastructure/main.tf
@@ -1,36 +1,37 @@
-resource "aws_instance" "example" {
-  ami           = "ami-0c94855ba95c71c99" # Amazon Linux 2 AMI (HVM), SSD Volume Type
-  instance_type = var.instanceType
 
-  vpc_security_group_ids = [
-    aws_security_group.example.id
-  ]
+# main.tf
 
-  user_data = <<-EOF
-    #!/bin/bash
-    # Install Docker
-    amazon-linux-extras install docker -y
-    systemctl start docker
-    usermod -a -G docker ec2-user
-
-    # Pull and run the Docker container
-    docker run -d -p 80:3000 YOUR_DOCKER_IMAGE
-  EOF
-
-  tags = {
-    Name = "example-instance"
-  }
+# Define the Terraform provider
+provider "random" {
+  # Configuration options (if any) can go here
 }
 
-resource "aws_security_group" "example" {
-  name        = "example-security-group"
-  description = "Allow inbound traffic on port 80"
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+# Generate a random string
+resource "random_string" "example" {
+  length  = 16
+  special = true
 }
 
+# Generate a random integer
+resource "random_integer" "example" {
+  min = 1
+  max = 100
+}
+
+# Generate a random pet name
+resource "random_pet" "example" {
+  length = 3
+}
+
+# Output the random values
+output "random_string" {
+  value = random_string.example.result
+}
+
+output "random_integer" {
+  value = random_integer.example.result
+}
+
+output "random_pet_name" {
+  value = random_pet.example.id
+}
--- simple-ec2-template/infrastructure/outputs.tf
+++ simple-ec2-template/infrastructure/outputs.tf
@@ -1,4 +1,4 @@
-output "public_ip" {
-  value       = aws_instance.example.public_ip
-  description = "The public IP address of the EC2 instance"
-}
+# output "public_ip" {
+#   value       = aws_instance.example.public_ip
+#   description = "The public IP address of the EC2 instance"
+# }
